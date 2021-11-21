5de49fe84c43a0fadc0cc79f3a83a6441c404392
changtimwu <changtimwu@gmail.com>
2021-11-21 18:35:24 +0800

    check env to access repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh -l
+ctid="dkact"
+echo "env="
+env
+echo "pwd="
+pwd
+echo "GITHUB_WORKSPACE=$GITHUB_WORKSPACE"
+echo "list files  "
+ls -al $GITHUB_WORKSPACE
+ls -al /github/workspace
+
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"

744304f2f3d47ec6ba9a67da7e7df50e0dddf5db
changtimwu <changtimwu@gmail.com>
2021-11-21 20:13:34 +0800

    accept directory as argument to perform rknn model conversion & eval

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,11 @@
 #!/bin/sh -l
-ctid="dkact"
-echo "env="
 env
-echo "pwd="
-pwd
-echo "GITHUB_WORKSPACE=$GITHUB_WORKSPACE"
-echo "list files  "
-ls -al $GITHUB_WORKSPACE
-ls -al /github/workspace
+wkdir=$1
+cd ${GITHUB_WORKSPACE}/${wkdir}
+python3 test.py
 
-echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
-lscpu
 cpuinfo=$(lscpu |grep "Model name")
 echo "cpuinfo=$cpuinfo"
 echo "::set-output name=cpuinfo::$cpuinfo"
